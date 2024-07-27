8a35d54da30e12315f176a82b34e87eec0bc7769
thymata44 <divermata@gmail.com>
2024-07-27 16:03:46 +0300

    cycle_task1

---
+++ cycle1.rb
@@ -0,0 +1,13 @@
+num = gets.chomp.to_i
+puts (0..num).sum
+p (0..num).to_a
+# puts 'Enter your N'
+# num = []
+# enter_num = gets.chomp.to_i
+# count = 0
+# while count <= enter_num do
+#   num << count
+#   count += 1
+# end
+# p num
+# puts num.sum
